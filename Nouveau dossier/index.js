89112f772e3d293942d6e0afd68a966f1b2e0f11
Daitan9 <lili4nviste@gmail.com>
2022-06-11 11:28:39 +0200

    First commit

---
+++ Nouveau dossier/index.js
@@ -0,0 +1,77 @@
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+function show_popup()
+{
+  document.getElementById("popup").focus();
+  document.getElementById('popup').style.display = 'block';
+  document.getElementById('popup').style.boxShadow = "0 0 0 10000px rgba(0,0,0,0.5)"
+}
+
+function close_popup()
+{
+  document.getElementById('popup').style.display = 'none'; 
+}
+
+function Discord() {
+
+  navigator.clipboard.writeText("Daitan#6606");
+  
+  alert("Pseudo Discord copié !");
+}
