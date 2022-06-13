cec063c96eceba613e21a8f2fe666ded16a158f3
Daitan9 <lili4nviste@gmail.com>
2022-06-13 10:45:41 +0200

    Ajout des images v2
    Ajout des images et du nouveau système de slideshow qui pourrait être amélioré d'ailleurs

--- site/index.js
+++ site/index.js
@@ -38,25 +38,7 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-
+//Pop-up
 function show_popup()
 {
   document.getElementById("popup").focus();
@@ -74,4 +56,27 @@ function Discord() {
   navigator.clipboard.writeText("Daitan#6606");
   
   alert("Pseudo Discord copié !");
+}
+
+//Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
 }
