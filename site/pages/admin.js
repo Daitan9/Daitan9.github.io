f8b9a54ee250bbd21d6191d5934280fb1186a64b
Daitan <lili4nviste@gmail.com>
2022-07-10 14:10:45 +0200

    Added Admin page

---
+++ site/pages/admin.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    //$('#username').focus();
+  
+    $("#submit").click(function () {
+      event.preventDefault(); // prevent PageReLoad
+  
+      var ValidEmail = $("#username").val() === "idlaviv"; // User validate
+      var ValidPassword = $("#password").val() === "3"; // Password validate
+  
+      if (ValidEmail === true && ValidPassword === true) {
+        // if ValidEmail & ValidPassword
+        $(".valid").css("display", "block");
+        window.location = "https://google.com"; // go to home.html
+      } else {
+        $(".error").css("display", "block"); // show error msg
+      }
+    });
+  });
