f5fc611d0d7841ab848e9b4bc0db3da7b9c371cf
Daitan <vistelilian@gmail.com>
2022-08-22 17:59:40 +0200

    uiuiui

--- site/Admin/profs.js
+++ site/Admin/profs.js
@@ -58,29 +58,6 @@ function Discord() {
   alert("Pseudo Discord copié !");
 }
 
-//Slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
-
 let adminAccess = localStorage.getItem('adminaccess'); 
 
 if (adminAccess === 'yes') {
@@ -118,3 +95,20 @@ if (adminAccess === 'yes') {
     document.getElementsByClassName('error2').style.display = 'none';
     document.getElementsByClassName('valid2').style.display = 'none';
   }
+
+  // function change_image()
+  // {
+  //   var audio = document.getElementById("audio");
+
+  //   if (document.getElementById("SG1").style.display === "inline") {
+  //     document.getElementById("SG1").style.display="none";
+  //     document.getElementById("SG2").style.display="inline";
+  //     audio.play();
+  //   } 
+  //   else {
+  //     document.getElementById("SG1").style.display="inline";
+  //     document.getElementById("SG2").style.display="none";
+  //     audio.pause();
+  //     audio.currentTime = 0;
+  //   }
+  // }
